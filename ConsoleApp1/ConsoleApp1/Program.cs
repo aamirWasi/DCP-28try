03f74d44ac9d5dabf76e9104f7148e98fbca07eb
aamir <wasi3aamir@gmail.com>
2019-02-02 14:11:43 +0600

    Not Accepted

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int t = 0; t < T; t++)
+            {
+                string line = Console.ReadLine();
+                string[] space = line.Split(' ');
+                double num1 = double.Parse(space[0]);
+                double num2 = double.Parse(space[1]);
+                double a = (2 * num1);
+                double Dc = Math.Round(a,2);
+                double Sc = Math.Round(num2*Math.Round(Math.Sqrt(2),2),2);
+                double Ss = Math.Round(Sc);
+                if (Dc == Ss)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            }
+        }
+    }
+}
